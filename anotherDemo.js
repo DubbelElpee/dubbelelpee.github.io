e17738a84411d9bd13cb721941afec14dcdf61ca
Luc Peersman <dubbelelpee@outlook.com>
2022-02-07 22:41:10 +0100

    div

--- anotherDemo.js
+++ anotherDemo.js
@@ -1,2 +1,3 @@
 alert('Just another demo');
 document.getElementById("output").innerHTML = "Beeeee baaa booo JavaScript";
+
--- namesDemo.js
+++ namesDemo.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("output").innerHTML = "Luc P";
   }
+/*
+  function copyInfo(){
+    //document.getElementById("output2").innerHTML = document.getElementById("name").innerHTML;
+    document.getElementById("output2").innerHTML ="foo";
+  }
+  */
