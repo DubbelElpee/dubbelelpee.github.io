454e651eb4883d6fced1a2123178f8348ed73c8b
Luc Peersman <dubbelelpee@outlook.com>
2022-02-13 13:53:07 +0100

    bull

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 function meow(){
     
-    document.getElementById("output2").innerHTML = "main.meow is called";
+    document.getElementById("output2").innerHTML = "meow is called";
 
 }
 
 function arf(){
     document.getElementById("output2").innerHTML = "arf arf";
+
+}
+
+function moo(){
+    document.getElementById("output2").innerHTML = "moo";
 
 }
