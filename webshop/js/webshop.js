54624a80ae5945eb971d7df570f69352819b2300
Luc Peersman <dubbelelpee@outlook.com>
2022-02-21 23:06:53 +0100

    checkout

--- webshop/js/webshop.js
+++ webshop/js/webshop.js
@@ -6,8 +6,11 @@ var bin_s=7;
 var stapler_s=4;
 var pencilholder_s =5;
 
-function loaded(){
-   
+var bin_p=0;
+var stapler_p=0;
+var pencilholder_p=0;
+
+function loaded(){   
 
     document.getElementById("p1").innerText="Price "+bin;
     document.getElementById("p2").innerText="Price "+stapler;
@@ -21,18 +24,25 @@ function loaded(){
 
 function binBuy() {
     console.log('bin');
+    bin_p++;
     bin_s--;
     document.getElementById("s1").innerText="Stock "+bin_s;
 
 }
 
 function staplerBuy(){
+    stapler_p++;
     stapler_s--;
     document.getElementById("s2").innerText="Stock "+stapler_s;
 }
 
 function pencilholderBuy(){
-
+    pencilholder_p++;
     pencilholder_s--;
     document.getElementById("s3").innerText="Stock "+pencilholder_s;
 }
+
+function checkoutLoaded(){
+    alert("hi");
+}
+
