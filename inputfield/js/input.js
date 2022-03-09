e7b759b5fe4d98241cb0a4626518237d8d387e72
Luc Peersman <dubbelelpee@outlook.com>
2022-03-09 21:31:46 +0100

    loops added

--- inputfield/js/input.js
+++ inputfield/js/input.js
@@ -1,3 +1,6 @@
+var fibo1=1;
+var fibo2=1;
+
 function clickPress1(event) {
   if (event.keyCode == 13) {
     showValue(); 
@@ -29,5 +32,36 @@ function clickPress3(event) {
 
 function convert(){
   let inputValue3 = document.getElementById("inputField3").value;
-  document.getElementById("p3").innerText = "Euro's"+1.2*inputValue3;
+  document.getElementById("p3").innerText = "Euro's = "+Math.round(120*inputValue3)/100;
+}
+
+function multiply(){
+  let inputValue4 = document.getElementById("inputField4").value;
+  let inputValue5 = document.getElementById("inputField5").value;
+  document.getElementById("p4").innerText = "Product = "+inputValue4*inputValue5;
+}
+
+function toFahrenheid(){
+  let inputValue6 = document.getElementById("inputField6").value;
+  document.getElementById("p5").innerText = "Fahrenheid = "+((9/5)*inputValue6+32);
+}
+
+function area(){
+  let inputValue7 = document.getElementById("inputField7").value;
+  document.getElementById("p6").innerText = "Area = "+inputValue7*inputValue7*Math.PI;
+}
+
+function average(){
+  let inputValue8 = document.getElementById("inputField8").value;
+  let inputValue9 = document.getElementById("inputField9").value;
+  document.getElementById("p7").innerText = (inputValue8*1.0+inputValue9*1.0)/2;
+
+}
+
+function fibo(){
+  let fibo=fibo1+fibo2;
+  document.getElementById("p8").innerText = fibo;
+
+  fibo1=fibo2;
+  fibo2=fibo;
 }
---
+++ loops/js/loopsDemo.js
@@ -0,0 +1,30 @@
+function count10(){
+    console.clear();
+    for(var i=0;i<10;i++){
+       
+        console.log(i+1);
+    }
+}
+
+function countDown10(){
+    console.clear();
+    for(var i=0;i<10;i++){
+        
+        console.log(20-i);
+    }
+}
+
+function count20(){
+    console.clear();
+    for(var i=0;i<10;i++){
+        
+        console.log(2*i+2);
+    }
+}
+
+function count100_90(){
+    console.clear();
+    for(var i=0;i<10;i++){
+        console.log(100-i);
+    }
+}
