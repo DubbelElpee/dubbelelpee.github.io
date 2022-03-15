09cc8e9abd04680f6c7dbb53bc414a9514fe01a8
Luc Peersman <dubbelelpee@outlook.com>
2022-03-15 21:10:01 +0100

    com

--- loops/js/loopsDemo.js
+++ loops/js/loopsDemo.js
@@ -1,9 +1,43 @@
+var ar=[44,-77,5];
+
+var nietmachine = {
+    "name": "nietmachine",
+    
+    "legs": 4,
+    "tails": 1,
+    "enemies": ["Water", "Dogs"],
+    eat(){console.log("eat")},
+    hurt(){this.legs--;},
+
+
+
+
+
+  };
+
 function count10(){
     console.clear();
+    cat.name="Qwerty";
+    cat.eat();
+    cat.hurt();
+    console.log("cat has now "+cat.legs + " legs");
+    
+
+
+    console.log(cat.name);
+    console.log(cat.legs);
+ 
     for(var i=0;i<10;i++){
        
         console.log(i+1);
+       
     }
+
+    for(var i=0;i<ar.length; i++){
+        if(ar[i] > 0) { ar[i] = "cat"; }	else { ar[i] = "dog"; }
+
+    }
+    console.log(ar);
 }
 
 function countDown10(){
