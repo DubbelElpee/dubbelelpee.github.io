cd4f65f3275c2704b470709ce12b859995308592
Luc Peersman <dubbelelpee@outlook.com>
2022-03-21 15:58:29 +0100

    webshop_OOP_array

---
+++ webshop_oop_constructor/js/webshop.js
@@ -0,0 +1,100 @@
+
+class Item {
+    constructor(n, p) {
+      this.name = n;
+      this.price = p;
+    }
+}
+
+var bin= new Item("bin",8);
+var stapler = new Item("stapler", 3);
+var pencilHolder = new Item("pencilholder", 2);
+
+var items=3;
+
+var binStock=5;
+var binPurchased=0;
+
+var staplerStock=3;
+var staplerPurchased=0;
+
+var pencilholderStock=8;
+var pencilholderPurchased=0
+
+var totalPrice = 0;
+
+function loaded() {
+    document.getElementById("p1").innerText = "Price " + bin.price;
+    document.getElementById("p2").innerText = "Price " + stapler.price;
+    document.getElementById("p3").innerText = "Price " + pencilHolder.price;
+
+    document.getElementById("s1").innerText = "Stock " + binStock;
+    document.getElementById("s2").innerText = "Stock " + staplerStock;
+    document.getElementById("s3").innerText = "Stock " + pencilholderStock;
+
+}
+
+function binBuy() {
+
+    console.log('bin');
+    binPurchased++;
+    binStock--;
+    document.getElementById("s1").innerText = "Stock " + binStock;
+
+    if (binStock == 0) {
+        document.getElementById("s1").innerText = "OUT OF STOCK";
+        document.getElementById("binBuyButton").style.display = "none";
+    }
+
+}
+
+function staplerBuy() {
+    if (staplerStock > 0) {
+        staplerPurchased++;
+        staplerStock--;
+        document.getElementById("s2").innerText = "Stock " + staplerStock;
+    }
+}
+
+function pencilholderBuy() {
+    pencilholderPurchased++;
+    pencilholderStock--;
+    document.getElementById("s3").innerText = "Stock " + pencilholderStock;
+}
+
+function checkOut() {
+    if (binPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = binPurchased + " bin(s) x " + bin.price + " = " + binPurchased * bin.price;
+        document.body.appendChild(para);
+
+        totalPrice += binPurchased * bin.price;
+    }
+
+    if (staplerPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = staplerPurchased + " stapler(s) x " + stapler.price + " = " + staplerPurchased * stapler.price;
+        document.body.appendChild(para);
+
+        totalPrice += staplerPurchased * stapler.price;
+    }
+
+    if (pencilholderPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = pencilholderPurchased + " pencilholder x " + pencilHolder.price + " = " + pencilholderPurchased * pencilHolder.price;
+        document.body.appendChild(para);
+
+        totalPrice += pencilholderPurchased * pencilHolder.price;
+    }
+
+    if (totalPrice > 0) {
+        const para = document.createElement("p");
+        para.innerHTML = "Sum: " + totalPrice + "<BR/>" + "VAT 21%: " + totalPrice * 0.21 + "<BR/>" + "TOTAL: " + totalPrice * 1.21;
+        document.body.appendChild(para);
+
+    }
+
+
+
+}
+
---
+++ webshop_oop_constructor_array/js/webshop.js
@@ -0,0 +1,93 @@
+
+class Item {
+    constructor(n, p, imp) {
+      this.name = n;
+      this.price = p;
+      this.imagePath =imp;
+    }
+}
+
+var items=[];
+
+// ADD ITEMS TO SHOP HERE
+items.push(new Item("bin",8, "img/bin.jpg"));
+items.push(new Item("stapler",3,  "img/stapler.jpg"));
+//items.push(new Item("pencilholder", 2, "img/pencilholder.jpg"));
+
+//ADD ITEMS IN STOCK FOR EACH ITEM:
+var stockedItems=[3,8,2]; //3 bins, 8 staplers, 2 pencilholders
+var puchasedItems=[0,0,0];
+
+var totalPrice = 0;
+
+function loaded() {
+    var targetDiv = document.getElementById('mainContent');
+
+    for(var i=0; i<items.length; i++){
+        console.log("LOOP #"+i);
+        const content = document.createElement("div");
+        content.innerHTML = '<div style="float:left">'+
+        '<img src='+items[i].imagePath+' /><br />'+
+        '<p id=p'+i+'>Price '+items[i].price+'</p>'+
+        '<p id=s'+i+'>In stock:'+stockedItems[i]+'</p>'+
+        '<button onclick="buy('+i+')" id="rr">Buy '+items[i].name+'</button>'+
+        '</div>';
+        targetDiv.appendChild(content);
+    }
+
+}
+
+function buy(it){
+    var i=it;
+    console.log("function buy, item ="+it);
+    stockedItems[i]--;
+    puchasedItems[i]++;
+    document.getElementById('s'+i).innerText="In stock "+stockedItems[i];
+}
+
+
+
+function checkOut() {
+    for(var i=0;i<items.length;i++){
+        totalPrice+=puchasedItems[i]*items[i].price;
+    }
+    document.getElementById("totalPrice").innerText="Totalprice = "+totalPrice;
+
+
+/*
+    if (binPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = binPurchased + " bin(s) x " + bin.price + " = " + binPurchased * bin.price;
+        document.body.appendChild(para);
+
+        totalPrice += binPurchased * bin.price;
+    }
+
+    if (staplerPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = staplerPurchased + " stapler(s) x " + stapler.price + " = " + staplerPurchased * stapler.price;
+        document.body.appendChild(para);
+
+        totalPrice += staplerPurchased * stapler.price;
+    }
+
+    if (pencilholderPurchased > 0) {
+        const para = document.createElement("p");
+        para.innerText = pencilholderPurchased + " pencilholder x " + pencilHolder.price + " = " + pencilholderPurchased * pencilHolder.price;
+        document.body.appendChild(para);
+
+        totalPrice += pencilholderPurchased * pencilHolder.price;
+    }
+
+    if (totalPrice > 0) {
+        const para = document.createElement("p");
+        para.innerHTML = "Sum: " + totalPrice + "<BR/>" + "VAT 21%: " + totalPrice * 0.21 + "<BR/>" + "TOTAL: " + totalPrice * 1.21;
+        document.body.appendChild(para);
+
+    }
+    */
+
+
+
+}
+
