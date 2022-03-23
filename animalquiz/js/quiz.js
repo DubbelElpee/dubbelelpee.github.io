845f909c45ebe31d93d5066b87865dbc2dbbf175
Luc Peersman <dubbelelpee@outlook.com>
2022-03-23 21:42:28 +0100

    animalquiz added

---
+++ animalquiz/js/quiz.js
@@ -0,0 +1,61 @@
+var inputEl;
+var feedbackEl;
+var scoreEl;
+var score;
+var animalIndex;
+
+var animals=[];
+
+class Animal {
+    constructor(n, i) {
+      this.name = n;
+      this.imagePath =i;
+    }
+}
+
+function load(){
+    animals.push(new Animal("Dog","img/dog.jpg"));
+    animals.push(new Animal("Cat", "img/cat.jpg"));
+    animals.push(new Animal("Donkey","img/donkey.jpg"));
+    animals.push(new Animal("Cow","img/cow.jpg"));
+    
+    score=0;
+    animalIndex=0;
+    showAnimal(0);
+    
+}
+
+function checkAnswer(){
+    inputEl=document.getElementById("answer");
+    feedbackEl=document.getElementById("feedback");
+    scoreEl=document.getElementById("score");
+    
+    feedbackEl.innerText="";
+    let inputValue = inputEl.value; 
+    
+    if(inputValue==animals[animalIndex].name){
+        
+        score++;
+        scoreEl.innerText="Your score : "+score;
+        animalIndex++;
+        inputEl.value= "";
+
+        showAnimal(animalIndex);
+
+    
+    }
+    else{
+        feedbackEl.innerText="Incorrect, the answer is "+animals[animalIndex].name+"! Try again!"
+    }
+}
+
+function showAnimal(i){
+    if(i==animals.length){
+        document.getElementById("Image").remove();
+        document.getElementById("feedback").innerText= "The end.. well done!";
+    }
+    document.getElementById("image").src=animals[i].imagePath;
+}
+    
+        
+    
